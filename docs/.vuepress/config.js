a5e3f815c790f2f8449fd0bb8fee8ff4291921c1
maomao1996 <1714487678@qq.com>
2019-12-09 11:50:01 +0800

    update : vuepress

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,12 +10,25 @@ module.exports = {
       description: "茂茂的开发笔记"
     }
   },
-  head: [["link", { rel: "icon", href: "/favicon.ico" }]],
+  head: [
+    ["link", { rel: "icon", href: "/favicon.ico" }],
+    ["link", { rel: "manifest", href: "/manifest.json" }],
+    ["meta", { name: "theme-color", content: "#3eaf7c" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    [
+      "meta",
+      { name: "apple-mobile-web-app-status-bar-style", content: "black" }
+    ],
+    ["link", { rel: "apple-touch-icon", href: "/favicon.ico" }],
+    ["link", { rel: "mask-icon", href: "/favicon.ico", color: "#3eaf7c" }],
+    ["meta", { name: "msapplication-TileImage", content: "/favicon.ico" }],
+    ["meta", { name: "msapplication-TileColor", content: "#000000" }]
+  ],
   themeConfig: {
+    repo: "maomao1996/mm-notes/",
     nav: [
       { text: "在线音乐播放器", link: "http://music.mtnhao.com/" },
-      { text: "Blog", link: "http://www.mtnhao.com/" },
-      { text: "Github", link: "https://github.com/maomao1996/mm-notes" }
+      { text: "Blog", link: "http://www.mtnhao.com/" }
     ],
     sidebar: [
       {
@@ -44,13 +57,23 @@ module.exports = {
         path: "/git/"
       }
     ],
-    lastUpdated: "更新时间"
+    lastUpdated: "上次更新"
   },
   plugins: [
     [
       "@vuepress/google-analytics",
       {
         ga: "UA-153242456-1"
+      }
+    ],
+    "@vuepress/back-to-top",
+    [
+      "@vuepress/pwa",
+      {
+        updatePopup: {
+          message: "发现新内容可用",
+          buttonText: "刷新"
+        }
       }
     ]
   ]
