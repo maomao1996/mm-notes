3d0a3f0f74061a05fc4a937d5ab3c267e51d9029
maomao1996 <1714487678@qq.com>
2020-02-01 18:41:13 +0800

    update : 格式化

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,85 +1,82 @@
 module.exports = {
-  base: "/mm-notes/",
-  dest: "./dist",
-  title: "茂茂的开发笔记",
-  description: "茂茂的开发笔记",
+  base: '/mm-notes/',
+  dest: './dist',
+  title: '茂茂的开发笔记',
+  description: '茂茂的开发笔记',
   locales: {
-    "/": {
-      lang: "zh-CN",
-      title: "茂茂的开发笔记",
-      description: "茂茂的开发笔记"
+    '/': {
+      lang: 'zh-CN',
+      title: '茂茂的开发笔记',
+      description: '茂茂的开发笔记'
     }
   },
   head: [
-    ["link", { rel: "icon", href: "/favicon.ico" }],
-    ["link", { rel: "manifest", href: "/manifest.json" }],
-    ["meta", { name: "theme-color", content: "#3eaf7c" }],
-    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
-    [
-      "meta",
-      { name: "apple-mobile-web-app-status-bar-style", content: "black" }
-    ],
-    ["link", { rel: "apple-touch-icon", href: "/favicon.ico" }],
-    ["link", { rel: "mask-icon", href: "/favicon.ico", color: "#3eaf7c" }],
-    ["meta", { name: "msapplication-TileImage", content: "/favicon.ico" }],
-    ["meta", { name: "msapplication-TileColor", content: "#000000" }]
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/favicon.ico' }],
+    ['link', { rel: 'mask-icon', href: '/favicon.ico', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/favicon.ico' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
   themeConfig: {
-    repo: "maomao1996/mm-notes/",
+    repo: 'maomao1996/mm-notes/',
     nav: [
-      { text: "在线音乐播放器", link: "http://music.mtnhao.com/" },
-      { text: "Blog", link: "http://www.mtnhao.com/" }
+      { text: '在线音乐播放器', link: 'http://music.mtnhao.com/' },
+      { text: 'Blog', link: 'http://www.mtnhao.com/' }
     ],
     sidebar: [
       {
-        title: "Vue",
+        title: 'Vue',
         collapsable: true,
-        path: "/vue/"
+        path: '/vue/'
       },
       {
-        title: "CSS",
+        title: 'CSS',
         collapsable: true,
-        children: ["css/", "css/skill", "css/scss"]
+        children: ['css/', 'css/skill', 'css/scss']
       },
       {
-        title: "工具方法",
+        title: '工具方法',
         collapsable: true,
-        children: ["utils/regexp", "utils/function"]
+        children: ['utils/regexp', 'utils/function']
       },
       {
-        title: "Node 相关",
+        title: 'Node 相关',
         collapsable: true,
-        children: ["node/npm", "node/depend"]
+        children: ['node/npm', 'node/depend']
       },
       {
-        title: "Git",
+        title: 'Git',
         collapsable: true,
-        path: "/git/"
+        path: '/git/'
       },
       {
-        title: "软件相关",
+        title: '软件相关',
         collapsable: true,
-        children: ["software/mac"]
+        children: ['software/mac']
       }
     ],
-    lastUpdated: "上次更新"
+    lastUpdated: '上次更新'
   },
   plugins: [
     [
-      "@vuepress/google-analytics",
+      '@vuepress/google-analytics',
       {
-        ga: "UA-153242456-1"
+        ga: 'UA-153242456-1'
       }
     ],
-    "@vuepress/back-to-top",
+    '@vuepress/back-to-top',
     [
-      "@vuepress/pwa",
+      '@vuepress/pwa',
       {
         updatePopup: {
-          message: "发现新内容可用",
-          buttonText: "刷新"
+          message: '发现新内容可用',
+          buttonText: '刷新'
         }
       }
     ]
   ]
-};
+}
