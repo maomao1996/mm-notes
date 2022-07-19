5b0f00b8efe920354dec37ae9884aa38e37be04a
maomao1996 <1714487678@qq.com>
2022-07-19 23:39:50 +0800

    update(interview/concept): 模块化

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -127,6 +127,10 @@ export default defineUserConfig<DefaultThemeOptions>({
             '/interview/base/conversions.md'
           ]
         },
+        {
+          text: '概念知识',
+          children: ['/interview/concept/module.md']
+        },
         '/interview/es6/',
         '/interview/browser/',
         '/interview/coding/'
