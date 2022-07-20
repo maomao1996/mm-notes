87b80cf17d9b321a8fcc896de1260838fb7bd998
maomao1996 <1714487678@qq.com>
2022-07-20 20:27:06 +0800

    build: 升级 vuepress

---
+++ docs/.vuepress/configs/sidebar.ts
@@ -0,0 +1,70 @@
+import type { SidebarConfig } from '@vuepress/theme-default'
+
+export const sidebar: SidebarConfig = {
+  '/interview/': [
+    {
+      text: 'JavaScript 基础知识',
+      collapsible: true,
+      children: ['/interview/base/types.md', '/interview/base/conversions.md']
+    },
+    '/interview/es6/',
+    {
+      text: '概念知识',
+      collapsible: true,
+      children: ['/interview/concept/module.md']
+    },
+    '/interview/browser/',
+    '/interview/coding/'
+  ],
+  '/online-tools/': [
+    {
+      text: '在线工具',
+      children: ['/online-tools/', '/online-tools/bookmark-scripts.md']
+    }
+  ],
+  '/utils/': [
+    {
+      text: '工具方法',
+      children: ['/utils/regexp.md', '/utils/function.md', '/utils/library.md']
+    }
+  ],
+  '/vue/': [
+    {
+      text: 'Vue',
+      link: '/vue/'
+    }
+  ],
+  '/css/': [
+    {
+      text: 'CSS',
+      children: ['/css/', '/css/skill.md', '/css/scss.md']
+    }
+  ],
+  '/node/': [
+    {
+      text: 'Node 相关',
+      children: ['/node/npm.md', '/node/toolkit.md']
+    }
+  ],
+  '/git/': ['/git/', '/git/command.md'],
+  '/software/': [
+    {
+      text: '软件推荐',
+      children: [
+        '/software/cross-platform.md',
+        '/software/mac.md',
+        '/software/windows.md',
+        '/software/browser.md',
+        '/software/vscode.md',
+        '/software/webstorm.md',
+        '/software/zsh.md'
+      ]
+    }
+  ],
+  '/issue/': [
+    {
+      text: '疑难杂症',
+      children: ['/issue/h5.md', '/issue/pc.md']
+    }
+  ]
+}
