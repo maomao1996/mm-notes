df5cdfed3e1191dd7ea189bb29780e4b7959a6e0
maomao1996 <1714487678@qq.com>
2022-07-20 20:27:06 +0800

    build: 升级 vuepress

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,234 +1,63 @@
-import type { DefaultThemeOptions } from 'vuepress'
-import { defineUserConfig } from 'vuepress'
-const { path } = require('@vuepress/utils')
+import { defineUserConfig } from '@vuepress/cli'
+import { viteBundler } from '@vuepress/bundler-vite'
+import { webpackBundler } from '@vuepress/bundler-webpack'
+import { defaultTheme } from '@vuepress/theme-default'
+import { searchPlugin } from '@vuepress/plugin-search'
+import { pwaPlugin } from '@vuepress/plugin-pwa'
+import { pwaPopupPlugin } from '@vuepress/plugin-pwa-popup'
+import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics'
+import { shikiPlugin } from '@vuepress/plugin-shiki'
+
+import { head, navbar, sidebar } from './configs'
 
 const isProduction = process.env.NODE_ENV === 'production'
 
 // 兼容 GitHub Pages 和 vercel 部署
-const base = process.env.BUILD_ENV ? process.env.BUILD_ENV : '/'
+const base = process.env.BUILD_ENV
+  ? (process.env.BUILD_ENV as `/${string}/`)
+  : '/'
 
-export default defineUserConfig<DefaultThemeOptions>({
+export default defineUserConfig({
   base,
   dest: './dist',
-  bundler: isProduction ? '@vuepress/webpack' : '@vuepress/vite',
+  bundler: isProduction ? webpackBundler() : viteBundler(),
   lang: 'zh-CN',
   title: '茂茂物语',
   description: '前端常用知识、踩坑记录、软件推荐等',
-  head: [
-    ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    [
-      'meta',
-      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }
-    ],
-    ['link', { rel: 'apple-touch-icon', href: '/favicon.ico' }],
-    ['link', { rel: 'mask-icon', href: '/favicon.ico', color: '#3eaf7c' }],
-    ['meta', { name: 'msapplication-TileImage', content: '/favicon.ico' }],
-    ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
-    [
-      'script',
-      {
-        src: isProduction
-          ? 'https://hm.baidu.com/hm.js?8092fab2f2adfc7938ba5b8885aef5b4'
-          : ''
-      }
-    ]
-  ],
-  theme: path.resolve(__dirname, 'theme'),
-  themeConfig: {
+  head,
+  theme: defaultTheme({
     logo: '/logo.png',
-    navbar: [
-      {
-        text: '前端内卷不归路',
-        activeMatch: '^/interview',
-        link: '/interview/base/types.md'
-      },
-      {
-        text: '在线工具',
-        children: [
-          {
-            text: '网页工具',
-            link: '/online-tools/',
-            activeMatch: '/online-tools/$'
-          },
-          '/online-tools/bookmark-scripts.md'
-        ]
-      },
-      {
-        text: '工具方法',
-        children: [
-          '/utils/regexp.md',
-          '/utils/function.md',
-          '/utils/library.md'
-        ]
-      },
-      {
-        text: 'Vue / CSS / Node',
-        children: [
-          {
-            text: 'Node 相关',
-            children: ['/node/npm.md', '/node/toolkit.md']
-          },
-          {
-            text: 'CSS',
-            children: [
-              {
-                text: 'CSS 语法相关',
-                link: '/css/',
-                activeMatch: '/css/$'
-              },
-              '/css/skill.md',
-              '/css/scss.md'
-            ]
-          },
-          {
-            text: 'Vue',
-            children: ['/vue/']
-          },
-          {
-            text: '疑难杂症',
-            children: ['/issue/h5.md', '/issue/pc.md']
-          }
-        ]
-      },
-      {
-        text: 'Git',
-        activeMatch: '^/git',
-        link: '/git/README.md'
-      },
-      {
-        text: '软件推荐',
-        children: [
-          '/software/cross-platform.md',
-          '/software/mac.md',
-          '/software/windows.md',
-          '/software/browser.md',
-          '/software/vscode.md',
-          '/software/webstorm.md',
-          '/software/zsh.md'
-        ]
-      },
-      {
-        text: '主页和作品',
-        children: [
-          { text: 'Blog', link: 'https://www.fe-mm.com' },
-          { text: 'Github', link: 'https://github.com/maomao1996' },
-          { text: '在线音乐', link: 'https://netease-music.fe-mm.com' }
-        ]
-      }
-    ],
-    sidebar: {
-      '/interview/': [
-        {
-          text: 'JavaScript 基础知识',
-          children: [
-            '/interview/base/types.md',
-            '/interview/base/conversions.md'
-          ]
-        },
-        {
-          text: '概念知识',
-          children: ['/interview/concept/module.md']
-        },
-        '/interview/es6/',
-        '/interview/browser/',
-        '/interview/coding/'
-      ],
-      '/online-tools/': [
-        {
-          text: '在线工具',
-          children: ['/online-tools/', '/online-tools/bookmark-scripts.md']
-        }
-      ],
-      '/utils/': [
-        {
-          text: '工具方法',
-          children: [
-            '/utils/regexp.md',
-            '/utils/function.md',
-            '/utils/library.md'
-          ]
-        }
-      ],
-      '/vue/': [
-        {
-          text: 'Vue',
-          link: '/vue/'
-        }
-      ],
-      '/css/': [
-        {
-          text: 'CSS',
-          children: ['/css/', '/css/skill.md', '/css/scss.md']
-        }
-      ],
-      '/node/': [
-        {
-          text: 'Node 相关',
-          children: ['/node/npm.md', '/node/toolkit.md']
-        }
-      ],
-      '/git/': ['/git/', '/git/command.md'],
-      '/software/': [
-        {
-          text: '软件推荐',
-          children: [
-            '/software/cross-platform.md',
-            '/software/mac.md',
-            '/software/windows.md',
-            '/software/browser.md',
-            '/software/vscode.md',
-            '/software/webstorm.md',
-            '/software/zsh.md'
-          ]
-        }
-      ],
-      '/issue/': [
-        {
-          text: '疑难杂症',
-          children: ['/issue/h5.md', '/issue/pc.md']
-        }
-      ]
-    },
+    navbar,
+    sidebar,
     lastUpdatedText: '上次更新',
-    contributors: false
-  },
+    contributors: false,
+    notFound: [
+      '这里什么都没有',
+      '我们怎么到这来了？',
+      '这是一个 404 页面',
+      '看起来我们进入了错误的链接'
+    ],
+    backToHome: '返回首页'
+  }),
   plugins: [
-    [
-      '@vuepress/plugin-search',
-      {
-        locales: {
-          '/': {
-            placeholder: '搜索'
-          }
+    searchPlugin({
+      locales: {
+        '/': {
+          placeholder: '搜索'
         }
       }
-    ],
-    [
-      '@vuepress/plugin-google-analytics',
-      {
-        id: 'UA-153242456-1'
-      }
-    ],
-    ['@vuepress/plugin-pwa'],
-    [
-      '@vuepress/plugin-pwa-popup',
-      {
-        locales: {
-          '/': {
-            message: '发现新内容可用',
-            buttonText: '刷新'
-          }
+    }),
+    pwaPlugin(),
+    pwaPopupPlugin({
+      locales: {
+        '/': {
+          message: '发现新内容可用',
+          buttonText: '刷新'
         }
       }
-    ],
-    [
-      '@vuepress/plugin-shiki',
-      {
-        // https://github.com/shikijs/shiki/blob/main/docs/themes.md
-        theme: 'dark-plus'
-      }
-    ]
+    }),
+    isProduction ? googleAnalyticsPlugin({ id: 'UA-153242456-1' }) : [],
+    // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+    isProduction ? shikiPlugin({ theme: 'dark-plus' }) : []
   ]
 })
---
+++ docs/.vuepress/configs/navbar.ts
@@ -0,0 +1,78 @@
+import type { NavbarConfig } from '@vuepress/theme-default'
+
+export const navbar: NavbarConfig = [
+  {
+    text: '前端内卷不归路',
+    activeMatch: '^/interview',
+    link: '/interview/base/types.md'
+  },
+  {
+    text: '在线工具',
+    children: [
+      {
+        text: '网页工具',
+        link: '/online-tools/',
+        activeMatch: '/online-tools/$'
+      },
+      '/online-tools/bookmark-scripts.md'
+    ]
+  },
+  {
+    text: '工具方法',
+    children: ['/utils/regexp.md', '/utils/function.md', '/utils/library.md']
+  },
+  {
+    text: 'Vue / CSS / Node',
+    children: [
+      {
+        text: 'Node 相关',
+        children: ['/node/npm.md', '/node/toolkit.md']
+      },
+      {
+        text: 'CSS',
+        children: [
+          {
+            text: 'CSS 语法相关',
+            link: '/css/',
+            activeMatch: '/css/$'
+          },
+          '/css/skill.md',
+          '/css/scss.md'
+        ]
+      },
+      {
+        text: 'Vue',
+        children: ['/vue/']
+      },
+      {
+        text: '疑难杂症',
+        children: ['/issue/h5.md', '/issue/pc.md']
+      }
+    ]
+  },
+  {
+    text: 'Git',
+    activeMatch: '^/git',
+    link: '/git/README.md'
+  },
+  {
+    text: '软件推荐',
+    children: [
+      '/software/cross-platform.md',
+      '/software/mac.md',
+      '/software/windows.md',
+      '/software/browser.md',
+      '/software/vscode.md',
+      '/software/webstorm.md',
+      '/software/zsh.md'
+    ]
+  },
+  {
+    text: '主页和作品',
+    children: [
+      { text: 'Blog', link: 'https://www.fe-mm.com' },
+      { text: 'Github', link: 'https://github.com/maomao1996' },
+      { text: '在线音乐', link: 'https://netease-music.fe-mm.com' }
+    ]
+  }
+]
