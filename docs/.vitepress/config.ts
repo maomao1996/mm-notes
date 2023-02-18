c261363acc3fb63e8657556feb2d56b0740fd01f
maomao <1714487678@qq.com>
2023-02-18 20:01:08 +0800

    build(vitepress): 配置中文

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -10,6 +10,15 @@ export default defineConfig({
   description: '茂茂的成长之路，包含前端常用知识、源码阅读笔记、各种奇淫技巧、日常提效工具等',
   head,
 
+  lastUpdated: true,
+  cleanUrls: true,
+
+  /* markdown 配置 */
+  markdown: {
+    lineNumbers: true
+  },
+
+  /* 主题配置 */
   themeConfig: {
     i18nRouting: false,
 
@@ -17,6 +26,11 @@ export default defineConfig({
 
     nav,
     sidebar,
+    /* 右侧大纲配置 */
+    outline: {
+      level: 'deep',
+      label: '本页目录'
+    },
 
     socialLinks: [{ icon: 'github', link: 'https://github.com/maomao1996' }],
 
@@ -24,16 +38,57 @@ export default defineConfig({
       copyright: 'Copyright © 2019-present maomao'
     },
 
+    lastUpdatedText: '上次更新',
+
+    /* Algolia DocSearch 配置 */
     algolia: {
       appId: 'UVB82VCF2Y',
       apiKey: '4c67197afadb283c77e084bd50618c60',
-      indexName: 'fe-mm'
+      indexName: 'fe-mm',
+      placeholder: '搜索',
+      translations: {
+        button: {
+          buttonText: '搜索',
+          buttonAriaLabel: '搜索'
+        },
+        modal: {
+          searchBox: {
+            resetButtonTitle: '清除查询条件',
+            resetButtonAriaLabel: '清除查询条件',
+            cancelButtonText: '取消',
+            cancelButtonAriaLabel: '取消'
+          },
+          startScreen: {
+            recentSearchesTitle: '搜索历史',
+            noRecentSearchesText: '没有搜索历史',
+            saveRecentSearchButtonTitle: '保存至搜索历史',
+            removeRecentSearchButtonTitle: '从搜索历史中移除',
+            favoriteSearchesTitle: '收藏',
+            removeFavoriteSearchButtonTitle: '从收藏中移除'
+          },
+          errorScreen: {
+            titleText: '无法获取结果',
+            helpText: '你可能需要检查你的网络连接'
+          },
+          footer: {
+            selectText: '选择',
+            navigateText: '切换',
+            closeText: '关闭',
+            searchByText: '搜索提供者'
+          },
+          noResultsScreen: {
+            noResultsText: '无法找到相关结果',
+            suggestedQueryText: '你可以尝试查询',
+            reportMissingResultsText: '你认为该查询应该有结果？',
+            reportMissingResultsLinkText: '点击反馈'
+          }
+        }
+      }
     },
 
-    outline: 'deep'
-  },
-
-  markdown: {
-    lineNumbers: true
+    docFooter: {
+      prev: '上一篇',
+      next: '下一篇'
+    }
   }
 })
