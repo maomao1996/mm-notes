7b9ac879d7966248a8e2cb05edc5c4407966222f
maomao <1714487678@qq.com>
2023-02-20 20:38:05 +0800

    build(vitepress): 添加注销 PWA 服务代码

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -7,6 +7,28 @@ import AsideSponsors from './components/AsideSponsors.vue'
 
 import './styles/index.scss'
 
+if (typeof window !== 'undefined') {
+  /* 注销 PWA 服务 */
+  if (window.navigator && navigator.serviceWorker) {
+    navigator.serviceWorker.getRegistrations().then(function (registrations) {
+      for (let registration of registrations) {
+        registration.unregister()
+      }
+    })
+  }
+
+  /* 删除浏览器中的缓存 */
+  if ('caches' in window) {
+    caches.keys().then(function (keyList) {
+      return Promise.all(
+        keyList.map(function (key) {
+          return caches.delete(key)
+        })
+      )
+    })
+  }
+}
+
 export default Object.assign({}, Theme, {
   Layout: () =>
     h(Theme.Layout, null, {
