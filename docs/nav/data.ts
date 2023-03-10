15a78010e1bd5e855b595273c2c354a34a2e7941
maomao <1714487678@qq.com>
2023-03-10 19:50:18 +0800

    update(nav): 更新导航站点数据

--- docs/nav/data.ts
+++ docs/nav/data.ts
@@ -36,6 +36,35 @@ export const NAV_DATA: NavData[] = [
     ]
   },
   {
+    title: '茂茂的站点导航',
+    items: [
+      {
+        icon: '/logo.png',
+        title: '前端日常笔记',
+        desc: '日常笔记记录（零零散散啥都记系列）',
+        link: 'https://github.com/maomao1996/daily-notes'
+      },
+      {
+        icon: '/logo.png',
+        title: '前端思维导图',
+        desc: '用思维导图的方式总结个人所学知识',
+        link: 'https://mindmap.fe-mm.com'
+      },
+      {
+        icon: 'https://qwerty.fe-mm.com/apple-touch-icon.png',
+        title: 'Qwerty Learner',
+        desc: '为键盘工作者设计的单词记忆与英语肌肉记忆锻炼软件',
+        link: 'https://qwerty.fe-mm.com'
+      },
+      {
+        icon: '/logo.png',
+        title: 'mmPlayer',
+        desc: 'mmPlayer 在线音乐播放器',
+        link: 'https://netease-music.fe-mm.com'
+      }
+    ]
+  },
+  {
     title: 'React 生态',
     items: [
       {
@@ -403,6 +432,29 @@ export const NAV_DATA: NavData[] = [
         title: 'Docusaurus',
         desc: '基于 React 的静态网站生成器',
         link: 'https://docusaurus.io/zh-CN'
+      }
+    ]
+  },
+  {
+    title: '图标库',
+    items: [
+      {
+        icon: 'https://img.alicdn.com/imgextra/i4/O1CN01Z5paLz1O0zuCC7osS_!!6000000001644-55-tps-83-82.svg',
+        title: 'iconfont',
+        desc: '国内功能很强大且图标内容很丰富的矢量图标库，提供矢量图标下载、在线存储、格式转换等功能',
+        link: 'https://www.iconfont.cn'
+      },
+      {
+        icon: 'https://lf1-cdn2-tos.bytegoofy.com/bydesign/iconparksite/logo.svg',
+        title: 'IconPark 图标库',
+        desc: 'IconPark图标库是一个通过技术驱动矢量图标样式的开源图标库，可以实现根据单一 SVG 源文件变换出多种主题， 具备丰富的分类、更轻量的代码和更灵活的使用场景；致力于构建高质量、统一化、可定义的图标资源，让大多数人都能够选择适合自己的风格图标',
+        link: 'https://iconpark.oceanengine.com/official'
+      },
+      {
+        icon: 'https://emoji.muan.co/appicon.png',
+        title: 'Emoji searcher',
+        desc: 'Emoji 表情大全',
+        link: ''
       }
     ]
   },
