21cd8a9e0d4a0137612b66b7366df4e1bd89d8b7
maomao <1714487678@qq.com>
2023-04-08 22:48:48 +0800

    refactor(vitepress): 优化配置文件

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,7 +3,7 @@ import { resolve } from 'node:path'
 import { SitemapStream } from 'sitemap'
 import { defineConfig, PageData } from 'vitepress'
 
-import { head, nav, sidebar } from './configs'
+import { head, nav, sidebar, algolia } from './configs'
 
 const links: { url: string; lastmod: PageData['lastUpdated'] }[] = []
 
@@ -50,50 +50,7 @@ export default defineConfig({
     lastUpdatedText: '上次更新',
 
     /* Algolia DocSearch 配置 */
-    algolia: {
-      appId: 'UVB82VCF2Y',
-      apiKey: '4c67197afadb283c77e084bd50618c60',
-      indexName: 'fe-mm',
-      placeholder: '搜索',
-      translations: {
-        button: {
-          buttonText: '搜索',
-          buttonAriaLabel: '搜索'
-        },
-        modal: {
-          searchBox: {
-            resetButtonTitle: '清除查询条件',
-            resetButtonAriaLabel: '清除查询条件',
-            cancelButtonText: '取消',
-            cancelButtonAriaLabel: '取消'
-          },
-          startScreen: {
-            recentSearchesTitle: '搜索历史',
-            noRecentSearchesText: '没有搜索历史',
-            saveRecentSearchButtonTitle: '保存至搜索历史',
-            removeRecentSearchButtonTitle: '从搜索历史中移除',
-            favoriteSearchesTitle: '收藏',
-            removeFavoriteSearchButtonTitle: '从收藏中移除'
-          },
-          errorScreen: {
-            titleText: '无法获取结果',
-            helpText: '你可能需要检查你的网络连接'
-          },
-          footer: {
-            selectText: '选择',
-            navigateText: '切换',
-            closeText: '关闭',
-            searchByText: '搜索提供者'
-          },
-          noResultsScreen: {
-            noResultsText: '无法找到相关结果',
-            suggestedQueryText: '你可以尝试查询',
-            reportMissingResultsText: '你认为该查询应该有结果？',
-            reportMissingResultsLinkText: '点击反馈'
-          }
-        }
-      }
-    },
+    algolia,
 
     docFooter: {
       prev: '上一篇',
