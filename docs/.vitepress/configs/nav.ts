05e5b73d380699df0d9dc6fd94031ba6f0676f70
maomao <1714487678@qq.com>
2023-04-12 19:26:44 +0800

    feat(vitepress): 导航菜单调整

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -8,7 +8,9 @@ export const nav: DefaultTheme.Config['nav'] = [
       { text: 'JavaScript 基础知识', link: '/fe/javascript/types' },
       { text: 'ES6 常用知识', link: '/fe/es6/' },
       { text: 'TypeScript 基础知识', link: '/fe/typescript/base' },
-      { text: '浏览器相关知识', link: '/fe/browser/' }
+      { text: 'TypeScript 类型体操', link: '/fe/typescript/challenges' },
+      { text: '浏览器相关知识', link: '/fe/browser/' },
+      { text: '编程题', link: '/fe/coding/' }
     ],
     activeMatch: '^/fe'
   },
@@ -23,19 +25,23 @@ export const nav: DefaultTheme.Config['nav'] = [
       {
         text: '常用工具/方法',
         items: [
-          { text: '工具库整理', link: '/workflow/utils/library' },
+          { text: '常用工具库整理', link: '/workflow/utils/library' },
           { text: '常用正则整理', link: '/workflow/utils/regexp' },
           { text: '常用方法整理', link: '/workflow/utils/function' }
         ]
       },
       {
-        text: 'CSS 相关',
-        items: [
-          { text: 'CSS 语法', link: '/workflow/css/spec' },
-          { text: 'CSS 奇淫技巧', link: '/workflow/css/tricks' },
-          { text: 'Sass 常用技巧', link: '/workflow/sass/' }
-        ]
+        text: 'Tailwind CSS 使用与配置',
+        link: '/workflow/library/tailwindcss'
       },
+      // {
+      //   text: 'CSS 相关',
+      //   items: [
+      { text: 'CSS 语法', link: '/workflow/css/spec' },
+      { text: 'CSS 奇淫技巧', link: '/workflow/css/tricks' },
+      { text: 'Sass 常用技巧', link: '/workflow/sass/' },
+      // ]
+      // },
       {
         text: 'Vue 小技巧',
         link: '/workflow/vue/'
@@ -46,11 +52,11 @@ export const nav: DefaultTheme.Config['nav'] = [
       //   items: [
       { text: 'Zsh 配置', link: '/workflow/terminal/zsh' },
       { text: '命令行工具', link: '/workflow/terminal/toolkit' },
-      { text: 'Shell 命令', link: '/workflow/terminal/shell' },
+      // { text: 'Shell 命令', link: '/workflow/terminal/shell' },
       //   ]
       // },
-      { text: 'Git 相关技巧', link: '/workflow/git/' },
-      { text: 'Git 命令清单', link: '/workflow/git/command' }
+      { text: 'Git 相关技巧', link: '/workflow/git/' }
+      // { text: 'Git 命令清单', link: '/workflow/git/command' }
     ],
     activeMatch: '^/workflow'
   },
