872f1a5afd408e404cd80d80a6d6dabb70fdc361
maomao <1714487678@qq.com>
2023-05-07 12:26:40 +0800

    feat(vitepress): MAsideSponsors 组件新增 title

--- docs/.vitepress/theme/components/MAsideSponsors.vue
+++ docs/.vitepress/theme/components/MAsideSponsors.vue
@@ -1,23 +1,64 @@
 <script setup lang="ts">
 import { withBase } from 'vitepress'
 import { VPDocAsideSponsors } from 'vitepress/theme'
+import VPLink from 'vitepress/dist/client/theme-default/components/VPLink.vue'
 
 const data = [
   {
-    items: [{ img: withBase('/sponsor/wechat-color.jpg') }]
+    items: [
+      {
+        img: withBase('/sponsor/wechat-color.jpg'),
+        url: '/mao'
+      }
+    ]
   },
   {
-    items: [{ img: withBase('/sponsor/alipay-color.jpg') }]
+    items: [
+      {
+        img: withBase('/sponsor/alipay-color.jpg'),
+        url: '/mao'
+      }
+    ]
   }
 ]
 </script>
 
 <template>
+  <p class="m-sponsor-title">
+    鼓励作者：欢迎
+    <VPLink href="https://github.com/maomao1996/mm-notes" no-icon>star</VPLink>
+    或打赏犒劳
+  </p>
   <VPDocAsideSponsors :data="data" />
 </template>
 
-<style>
+<style lang="scss">
+.m-sponsor-title {
+  margin-top: 10px;
+  margin-bottom: 6px;
+  text-align: center;
+  font-size: 13px;
+  font-weight: 700;
+  color: var(--vp-c-brand);
+
+  .VPLink {
+    text-decoration: underline;
+
+    &:hover {
+      color: var(--vp-custom-block-tip-text);
+    }
+  }
+}
+
 .vp-sponsor-grid.medium .vp-sponsor-grid-image {
   max-height: 90px;
+  filter: grayscale(0.8);
+}
+
+.dark .vp-sponsor-grid-item:hover {
+  background: var(--vp-custom-block-info-bg);
+}
+.vp-sponsor-grid-item:hover .vp-sponsor-grid-image {
+  filter: none;
 }
 </style>
