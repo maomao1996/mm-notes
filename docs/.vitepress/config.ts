40866216fb8f1ea18f1e1ae884799374a83f200a
maomao <1714487678@qq.com>
2023-10-09 19:38:52 +0800

    refactor(vitepress): 使用内置的 sitemap 配置

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,11 +1,6 @@
-import { createWriteStream } from 'node:fs'
-import { resolve } from 'node:path'
-import { SitemapStream } from 'sitemap'
-import { defineConfig, PageData } from 'vitepress'
+import { defineConfig } from 'vitepress'
 
 import { head, nav, sidebar, algolia } from './configs'
-
-const links: { url: string; lastmod: PageData['lastUpdated'] }[] = []
 
 export default defineConfig({
   outDir: '../dist',
@@ -21,7 +16,7 @@ export default defineConfig({
 
   /* markdown 配置 */
   markdown: {
-    lineNumbers: true
+    lineNumbers: true,
   },
 
   /* 主题配置 */
@@ -35,14 +30,14 @@ export default defineConfig({
     /* 右侧大纲配置 */
     outline: {
       level: 'deep',
-      label: '本页目录'
+      label: '本页目录',
     },
 
     socialLinks: [{ icon: 'github', link: 'https://github.com/maomao1996' }],
 
     footer: {
       message: '如有转载或 CV 的请标注本站原文地址',
-      copyright: 'Copyright © 2019-present maomao'
+      copyright: 'Copyright © 2019-present maomao',
     },
 
     darkModeSwitchLabel: '外观',
@@ -54,24 +49,12 @@ export default defineConfig({
 
     docFooter: {
       prev: '上一篇',
-      next: '下一篇'
-    }
+      next: '下一篇',
+    },
   },
 
   /* 生成站点地图 */
-  transformHtml: (_, id, { pageData }) => {
-    if (!/[\\/]404\.html$/.test(id))
-      links.push({
-        url: pageData.relativePath.replace(/((^|\/)index)?\.md$/, '$2'),
-        lastmod: pageData.lastUpdated
-      })
+  sitemap: {
+    hostname: 'https://notes.fe-mm.com/',
   },
-  buildEnd: async ({ outDir }) => {
-    const sitemap = new SitemapStream({ hostname: 'https://notes.fe-mm.com/' })
-    const writeStream = createWriteStream(resolve(outDir, 'sitemap.xml'))
-    sitemap.pipe(writeStream)
-    links.forEach((link) => sitemap.write(link))
-    sitemap.end()
-    await new Promise((r) => writeStream.on('finish', r))
-  }
 })
