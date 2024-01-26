7ad6d6ccf6212b47d44938d792e516f74773d9fb
maomao <1714487678@qq.com>
2024-01-26 17:31:09 +0800

    build: 优化 tailwindcss 配置

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,6 @@
-import tailwind from 'tailwindcss'
-import tailwindConfig from '@femm/tailwind-config'
-import autoprefixer from 'autoprefixer'
-
 export default {
-  plugins: [
-    tailwind({
-      presets: [tailwindConfig],
-      content: ['./docs/.vitepress/**/*.{js,ts,vue}', './docs/**/*.{md,vue}'],
-    }),
-    autoprefixer(),
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
