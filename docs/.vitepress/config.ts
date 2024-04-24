9f3dfe0c44e5d83d41edf9787482151fa626a6a1
maomao <1714487678@qq.com>
2024-04-24 17:50:01 +0800

    build(vitepress): 优化主题配置

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -31,10 +31,11 @@ export default defineConfig({
 
     nav,
     sidebar,
+
     /* 右侧大纲配置 */
     outline: {
       level: 'deep',
-      label: '本页目录',
+      label: '目录',
     },
 
     socialLinks: [{ icon: 'github', link: 'https://github.com/maomao1996' }],
@@ -44,9 +45,13 @@ export default defineConfig({
       copyright: 'Copyright © 2019-present maomao',
     },
 
-    darkModeSwitchLabel: '外观',
-    returnToTopLabel: '返回顶部',
-    lastUpdatedText: '上次更新',
+    lastUpdated: {
+      text: '最后更新于',
+      formatOptions: {
+        dateStyle: 'short',
+        timeStyle: 'medium',
+      },
+    },
 
     /* Algolia DocSearch 配置 */
     algolia,
@@ -55,6 +60,12 @@ export default defineConfig({
       prev: '上一篇',
       next: '下一篇',
     },
+
+    returnToTopLabel: '回到顶部',
+    sidebarMenuLabel: '菜单',
+    darkModeSwitchLabel: '主题',
+    lightModeSwitchTitle: '切换到浅色模式',
+    darkModeSwitchTitle: '切换到深色模式',
   },
 
   /* 生成站点地图 */
